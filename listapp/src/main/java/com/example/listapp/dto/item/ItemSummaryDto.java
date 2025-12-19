9914dcf76383cbcb6c94f8fc77f1a5dc51fa2cd8
Lennart Schlotter <lennschlott@gmail.com>
2025-12-19 20:57:00 +0100

    fix: add imagePath to itemsummarydto

--- listapp/src/main/java/com/example/listapp/dto/item/ItemSummaryDto.java
+++ listapp/src/main/java/com/example/listapp/dto/item/ItemSummaryDto.java
@@ -2,4 +2,4 @@ package com.example.listapp.dto.item;
 
 import java.util.UUID;
 
-public record ItemSummaryDto(UUID id, String title, Integer position) {}
+public record ItemSummaryDto(UUID id, String title, Integer position, String imagePath) {}
