b28ab7a3cc7f64acca17f730b5964f14d84b1d7a
Lennart Schlotter <lennschlott@gmail.com>
2025-12-21 18:35:33 +0100

    fix: remove manual uuid handling in builder

--- listapp/src/test/java/com/example/listapp/util/builder/UserTestBuilder.java
+++ listapp/src/test/java/com/example/listapp/util/builder/UserTestBuilder.java
@@ -1,11 +1,8 @@
 package com.example.listapp.util.builder;
 
-import java.util.UUID;
-
 import com.example.listapp.entity.User;
 
 public class UserTestBuilder {
-    private UUID id = UUID.randomUUID();
     private String oauth2Provider = "google";
     private String oauth2Sub = "oauth-sub-123";
     private String name = "testuser";
@@ -13,11 +10,6 @@ public class UserTestBuilder {
 
     public static UserTestBuilder aUser() {
         return new UserTestBuilder();
-    }
-
-    public UserTestBuilder withId(UUID id) {
-        this.id = id;
-        return this;
     }
 
     public UserTestBuilder withName(String name) {
@@ -32,7 +24,6 @@ public class UserTestBuilder {
 
     public User build() {
         User user = new User();
-        user.setId(id);
         user.setOauth2Provider(oauth2Provider);
         user.setOauth2Sub(oauth2Sub);
         user.setName(name);
