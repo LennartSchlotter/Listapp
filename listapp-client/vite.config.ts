5e22a6b44707194b4ddc8138e0aa1f594a5b6bbb
Lennart Schlotter <lennschlott@gmail.com>
2025-12-30 14:13:50 +0100

    fix: update vite config

--- listapp-client/vite.config.ts
+++ listapp-client/vite.config.ts
@@ -6,7 +6,22 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  }
-})
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/oauth2': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/login': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/logout': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+});
