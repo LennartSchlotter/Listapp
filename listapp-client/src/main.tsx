6dc2e89567c26d80e556d4c71c5c9efba7927679
Lennart Schlotter <lennschlott@gmail.com>
2026-01-02 21:13:25 +0100

    fix: prettier styling

--- listapp-client/src/main.tsx
+++ listapp-client/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
-import {routeTree} from './routeTree.gen';
+import { routeTree } from './routeTree.gen';
 
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
