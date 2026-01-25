c89148a2c08bfd1efb30dad29946099701f47cfd
Lennart Schlotter <lennschlott@gmail.com>
2026-01-25 22:50:13 +0100

    fix: customoidcuser bugs

--- listapp/src/main/java/com/example/listapp/security/CustomOidcUser.java
+++ listapp/src/main/java/com/example/listapp/security/CustomOidcUser.java
@@ -8,12 +8,19 @@ import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 
 import com.example.listapp.entity.User;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public class CustomOidcUser extends DefaultOidcUser {
 
     /**
      * The User Entity to create a CustomOidcUser for.
      */
-    private final User user;
+    private final transient User user;
+
+    /**
+     * Version number for the serialized form of the class.
+     */
+    private static final long serialVersionUID = 1L;
 
     /**
      * Constructor for the Custom User.
@@ -22,6 +29,10 @@ public class CustomOidcUser extends DefaultOidcUser {
      * @param userInfo Response from the UserInfo endpoint.
      * @param userField The custom user.
      */
+    @SuppressFBWarnings(
+        value = "EI_EXPOSE_REP2",
+        justification = "JPA entity stored intentionally in principal"
+    )
     public CustomOidcUser(
         final Collection authorities,
         final OidcIdToken idToken,
@@ -36,7 +47,33 @@ public class CustomOidcUser extends DefaultOidcUser {
      * Getter for the User object.
      * @return the user.
      */
+    @SuppressFBWarnings(
+        value = "EI_EXPOSE_REP",
+        justification = "Security principal intentionally exposes Jpa entity"
+    )
     public User getUser() {
         return user;
+    }
+
+    /**
+     * Overrides the equals function to enable entity-equality checks.
+     */
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (!(object instanceof CustomOidcUser other)) {
+            return false;
+        }
+        return user.getId().equals(other.user.getId());
+    }
+
+    /**
+     * Overrides the hashCode function to enable retrieving a user's hash code.
+     */
+    @Override
+    public int hashCode() {
+        return user.getId().hashCode();
     }
 }
