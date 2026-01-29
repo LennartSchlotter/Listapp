3a4eccfa6d732db3e6461bd659b67969665e1a3b
Lennart Schlotter <lennschlott@gmail.com>
2026-01-29 23:47:35 +0100

    fix: remove unused frontend parts

--- listapp-client/src/components/LoginButton.tsx
+++
@@ -1,7 +0,0 @@
-export function LoginButton() {
-  const login = () => {
-    window.location.href = '/oauth2/authorization/google';
-  };
-
-  return <button onClick={login}>Login with Google</button>;
-}
